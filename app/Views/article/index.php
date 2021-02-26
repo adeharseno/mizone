fc1d1a7c633e691585508dfb0d51b2c20fbfb627
adeharseno <ade.harseno@gmail.com>
2021-02-26 14:48:01 +0700

    archive page

---
+++ app/Views/article/index.php
@@ -0,0 +1,228 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="description" content="Minuman Isotonik dengan Vitamin B3, B6, B12 bantu lo semangat terus!!">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link rel="icon" href="images/favicon.ico" type="image/ico">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="<?php echo base_url('css/all.min.css'); ?>">
+  <style>body{overflow:auto}.menu-item{display:flex;float:right;margin-right:50px}.menu-item ul{padding-right:20px;padding-left:0}.menu-item ul li{display:inline-block;padding:0 30px;list-style:none;vertical-align:top;font-weight:500;font-size:28px}.menu-item ul li a{color:#fff;transition:all .3s ease}.menu-item ul li a:hover{text-decoration:none;color:#fe5000}.menu-inside{visibility:hidden;opacity:0;height:0;width:0}.menu-inside.active{visibility:visible;opacity:1;height:auto;width:auto}.menu-inside li{display:block}.has-search .form-control-feedback{position:absolute;z-index:2;display:block;width:2.375rem;height:2.375rem;line-height:2.375rem;text-align:center;pointer-events:none;color:#aaa}.form-group.has-search input{background:0 0;border:2px solid #fff;border-radius:8px;color:#fff;font-weight:500;min-width:210px;padding-left:34px}.form-group.has-search input::-webkit-input-placeholder{color:#fff}.form-group.has-search input:-ms-input-placeholder{color:#fff}.form-group.has-search input::placeholder{color:#fff}.nav-bottom{display:none}.section-product{padding:160px 0;background-size:cover;background-position:center;background-repeat:no-repeat;color:#fff;position:relative}.product-showcase{position:absolute;left:0;bottom:-32vh}.btn{font-weight:500}.btn-secondary{background:#f36b21;border-radius:14px!important;letter-spacing:.6px}.btn-secondary:hover{background:#ff7d04;border-color:#ff7d04}.title-main{font-size:72px;text-transform:uppercase}.title-main-bold{font-size:72px;font-weight:700}.title-secondary{font-size:40px;font-weight:400;margin:0}.title-card{position:absolute;bottom:14px;left:14px;right:14px;z-index:1;font-size:36px;font-weight:500;margin:0;color:#fff}.title-footer{color:#fff;background:#1c4094;padding:6px 12px;text-transform:uppercase;display:inline-block;border-radius:14px}.section-product label{font-size:30px;font-weight:400}.section-product p{font-size:20px}.section-article{padding:60px 0;background-size:cover;background-position:top;background-repeat:no-repeat;color:#fff}.card{margin-bottom:25px;background:0 0;border:none}.card-img{width:100%;height:300px;background-size:cover;background-position:center;background-repeat:no-repeat;position:relative;margin-bottom:14px}.card-img:before{content:'';display:block;position:absolute;bottom:0;left:0;right:0;top:0;height:100%;width:100%;-webkit-transition:all .25s ease-in-out;transition:all .25s ease-in-out;z-index:1;background-image:-webkit-gradient(linear,left top,left bottom,color-stop(25%,rgba(0,0,0,0)),color-stop(70%,#1c4094));background-image:linear-gradient(rgba(0,0,0,0) 25%,#1c4094 70%)}.card-content{padding:14px}.link{float:right;color:#fff;font-size:22px}.footer{padding:40px 0;background:#f36b21}.footer ul li{list-style:none;display:inline-block;padding:12px 28px}.footer ul{margin:25px 0;padding:0}.footer p{font-size:20px;color:#fff}
+        .main-slider .slider-items .img-fluid {
+            height: 102vh!important;
+        }
+  </style>
+
+  <title>Mizone Isotonik | 100% Mizone, Semangat Terus!!</title>
+   <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KDQLRG4');</script>
+    <!-- End Google Tag Manager -->
+
+
+</head>
+<body>
+     <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KDQLRG4"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
+  <div class="loader">
+    <div class="loader-content">
+      <i class="spinner"></i>
+      <p>LOADING</p>
+    </div>
+  </div>
+  <header class="header-main">
+    <div class="row align-items-center">
+      <div class="col-12 col-md-4">
+        <a href="/"><div class="logo"></div></a>
+      </div>
+      <div class="col-12 col-md-8 text-right">
+          <div class="menu-item">
+              <ul>
+                  <li><a href="#">Produk</a></li>
+                  <li>
+                      <a href="#">Info Mizone</a>
+                      <ul class="menu-inside">
+                          <li><a href="#">Artikel</a></li>
+                          <li><a href="#">Acara</a></li>
+                      </ul>
+                  </li>
+                  <li><a href="#">#TeamMizone</a></li>
+                  <li><a href="#">#FAQ</a></li>
+              </ul>
+              <form class="d-flex">
+                <div class="form-group has-search">
+                    <span class="form-control-feedback"><img src="<?php echo base_url('images/new-assets/search.png'); ?>" alt="Search" /></span>
+                    <input type="text" class="form-control" placeholder="Cari...">
+                </div>
+              </form>
+          </div>
+          <!--<div class="menu">-->
+          <!--  <div class="line line-1"></div>-->
+          <!--  <div class="line line-2"></div>-->
+          <!--  <div class="line line-3"></div>-->
+          <!--</div>-->
+      </div>
+    </div>
+  </header>
+
+  <div class="section-article" style="background-image: url('<?php echo base_url('images/new-assets/bg-article.jpg'); ?>');">
+    <div class="container mt-5">
+      <div class="row mt-5">
+        <div class="col mt-5">
+          <h5 class="title-secondary mb-4">Artikel</h5>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 col-md-4">
+          <div class="card">
+              <div class="card-img" style="background-image: url('<?php echo base_url('images/new-assets/artikel1.jpg'); ?>');">
+                <a href="#"><h5 class="title-card">Tanjakan lebih diminati daripada berlari</h5></a>
+              </div>
+              <div class="card-content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, ullam? Cupiditate molestiae, repudiandae quos ratione architecto</p>
+                <a href="#" class="btn btn-secondary">Selengkapnya</a>
+              </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-4">
+          <div class="card">
+              <div class="card-img" style="background-image: url('<?php echo base_url('images/new-assets/artikel2.jpg'); ?>');">
+                <a href="#"><h5 class="title-card">Hebatnya pelari wanita Indonesia</h5></a>
+              </div>
+              <div class="card-content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, ullam? Cupiditate molestiae, repudiandae quos ratione architecto</p>
+                <a href="#" class="btn btn-secondary">Selengkapnya</a>
+              </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-4">
+          <div class="card">
+              <div class="card-img" style="background-image: url('<?php echo base_url('images/new-assets/artikel3.jpg'); ?>');">
+                <a href="#"><h5 class="title-card">Manfaat Isotonik Setiap Hari</h5></a>
+              </div>
+              <div class="card-content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, ullam? Cupiditate molestiae, repudiandae quos ratione architecto</p>
+                <a href="#" class="btn btn-secondary">Selengkapnya</a>
+              </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-4">
+          <div class="card">
+              <div class="card-img" style="background-image: url('<?php echo base_url('images/new-assets/Acara1.jpg'); ?>');">
+                <a href="#"><h5 class="title-card">The Activ' Yoga di Sudirman</h5></a>
+              </div>
+              <div class="card-content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, ullam? Cupiditate molestiae, repudiandae quos ratione architecto</p>
+                <a href="#" class="btn btn-secondary">Selengkapnya</a>
+              </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-4">
+          <div class="card">
+              <div class="card-img" style="background-image: url('<?php echo base_url('images/new-assets/Acara2.jpg'); ?>');">
+                <a href="#"><h5 class="title-card">Memperkenalkan #TeamMizone!</h5></a>
+              </div>
+              <div class="card-content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, ullam? Cupiditate molestiae, repudiandae quos ratione architecto</p>
+                <a href="#" class="btn btn-secondary">Selengkapnya</a>
+              </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-4">
+          <div class="card">
+              <div class="card-img" style="background-image: url('<?php echo base_url('images/new-assets/Acara3.jpg'); ?>');">
+                <a href="#"><h5 class="title-card">60K Surabaya Open, Kamu Siap?</h5></a>
+              </div>
+              <div class="card-content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, ullam? Cupiditate molestiae, repudiandae quos ratione architecto</p>
+                <a href="#" class="btn btn-secondary">Selengkapnya</a>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col text-center">
+          <h3 class="title-footer">BELI MIZONE DI</h3>
+          <ul>
+            <li><a href="#"><img src="<?php echo base_url('images/new-assets/tokopedia.png'); ?>" alt="Tokopedia"></a></li>
+            <li><a href="#"><img src="<?php echo base_url('images/new-assets/blibli.png'); ?>" alt="Blibli"></a></li>
+            <li><a href="#"><img src="<?php echo base_url('images/new-assets/shopee.png'); ?>" alt="Shopee"></a></li>
+            <br />
+            <li><a href="#"><img src="<?php echo base_url('images/new-assets/jd.png'); ?>" alt="JD"></a></li>
+            <li><a href="#"><img src="<?php echo base_url('images/new-assets/klikindomart.png'); ?>" alt="Klik"></a></li>
+            <li><a href="#"><img src="<?php echo base_url('images/new-assets/alfacart.png'); ?>" alt="Alfacart"></a></li>
+          </ul>
+          <h3 class="title-footer mt-4">TEMUKAN KAMI</h3>
+          <ul>
+            <li><a href="#"><img src="<?php echo base_url('images/new-assets/instagram.png'); ?>" alt="Instagram"></a></li>
+            <li><a href="#"><img src="<?php echo base_url('images/new-assets/facebook.png'); ?>" alt="Facebook"></a></li>
+            <li><a href="#"><img src="<?php echo base_url('images/new-assets/twitter.png'); ?>" alt="Twitter"></a></li>
+          </ul>
+          <p>COPYRIGHT MIZONE 2021</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <!--<script src="js/jquery-3.4.1.min.js"></script>-->
+  <script src="<?php echo base_url('js/jquery-3.5.1.min.js'); ?>"></script>
+  <script src="<?php echo base_url('js/popper.min.js'); ?>"></script>
+  <script src="<?php echo base_url('js/bootstrap.min.js'); ?>"></script>
+  <script src="<?php echo base_url('js/mediaelement-and-player.min.js'); ?>"></script>
+  <script src="<?php echo base_url('js/main.min.js'); ?>"></script>
+
+  <script>
+    var url = window.location.href;
+    // Get DIV
+    // Check if URL contains the keyword
+    if (url.search('product-list') > 0) {
+      // Display the message
+      $('.main-menu').addClass('active');
+      $('.menu').addClass('active');
+    }
+    $('video').mediaelementplayer({
+        enableAutosize: true,
+        alwaysShowControls: true,
+        defaultVideoWidth: 480,
+        defaultVideoHeight: 270,
+        videoWidth: -1,
+        videoHeight: -1,
+        audioWidth: 400,
+        audioHeight: 30,
+    });
+    <?php if(isset($sliders) && count($sliders) > 0 ): foreach($sliders as $s): $value = json_decode_table($s, default_language()); ?>
+      <?php if( !empty($value['video']) || !empty($value["video_url"]) ):?>
+        
+        $('#videocontent-<?php echo $value['id']; ?>').on("hidden.bs.modal", function (i) {
+            $(".main-slider").slick("slickPlay"),
+            $(this).find('#video1-<?php echo $value['id']; ?>').trigger('pause');
+           
+        });
+        
+        $('#videocontent-<?php echo $value['id']; ?>').on("show.bs.modal", function (i) {
+            $(".main-slider").slick("slickPause"),
+            $(this).find('#video1-<?php echo $value['id']; ?>').trigger('play');
+            $(this).find('#video1-<?php echo $value['id']; ?>')[0].setCurrentTime(0);
+            
+        });
+      <?php endif; ?>
+    <?php endforeach; endif; ?>
+      
+  
+  </script>
+</body>
+</html>
